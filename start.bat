f324038562554b3aa216d34ed0c622ac1c44ce06
LeonidBolshakov <bolleoa@gmail.com>
2025-05-06 10:43:56 +0800

    Улучшен bat файл

--- start.bat
+++ start.bat
@@ -1,3 +1,7 @@
 @echo off
-py set_EN_US_layout.py
-py main.py
+REM Ярлык свойства: cmd /c "c:\Program Files\keyboard\start.bat"
+REM Ярлык рабочая папка: "c:\Program Files\keyboard"start.bat
+REM Ярлык дополнительно: Запуск от имени администратора
+cd /d "%~dp0"
+".venv\Scripts\pythonw.exe" set_EN_US_layout.py
+".venv\Scripts\pythonw.exe" main.py
