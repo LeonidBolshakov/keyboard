55510430ed148158bd337fdc90e527406a7973b7
LeonidBolshakov <bolleoa@gmail.com>
2025-05-28 13:03:05 +0800

    Расширена обработка ложных нажатий клавиш

--- hotkeys.py
+++ hotkeys.py
@@ -15,12 +15,15 @@ class Hotkey:  # Описание горячей клавиши
     suppress: bool = False  # Признак, требуется подавлять стандартную обработку клавиши
 
 
-def my_signature():
+def my_signature(hotkey: str) -> None:
     """Вывод личной подписи"""
-    keyboard.write(C.TEXT_SIGN1)
-    keyboard.release(C.HOTKEY_MODIFIER)  # Гасим модификатор от нажатия горячей клавиши
-    keyboard.send(C.KEY_SIGN2)
-    keyboard.write(C.TEXT_SIGN3)
+    if not is_false_clicks(hotkey):
+        keyboard.write(C.TEXT_SIGN1)
+        keyboard.release(
+            C.HOTKEY_MODIFIER
+        )  # Гасим модификатор от нажатия горячей клавиши
+        keyboard.send(C.KEY_SIGN2)
+        keyboard.write(C.TEXT_SIGN3)
 
 
 HOTKEYS = (
@@ -32,7 +35,11 @@ HOTKEYS = (
     Hotkey(
         C.HOTKEY_TEL, lambda: write_key(C.HOTKEY_TEL, C.TEXT_WRITE_TEL), modifier=True
     ),
-    Hotkey(C.HOTKEY_MY_SIGNATURE, my_signature, modifier=True),
+    Hotkey(
+        C.HOTKEY_MY_SIGNATURE,
+        lambda: my_signature(C.HOTKEY_MY_SIGNATURE),
+        modifier=True,
+    ),
     Hotkey(
         C.HOTKEY_CHANGE_REGISTER,
         lambda: keyboard.send(C.KEY_CHANGE_REGISTER),
@@ -43,17 +50,28 @@ HOTKEYS = (
     ),
 )
 
-DICT_FALSE_KEY = {"4": "left", "8": "up", "6": "right", "2": "down"}
+DICT_FALSE_KEY = {
+    "1": "end",
+    "2": "down",
+    "3": "page down",
+    "4": "left",
+    "6": "right",
+    "7": "home",
+    "8": "up",
+    "9": "page up",
+}
 
 
 def write_key(hotkey: str, text: str) -> None:
+    if not is_false_clicks(hotkey):
+        keyboard.write(text)
+
+
+def is_false_clicks(hotkey: str) -> bool:
     # Фильтруем ложные нажатия
     false_key = DICT_FALSE_KEY.get(hotkey)
 
-    if false_key and keyboard.is_pressed(false_key):
-        return
-
-    keyboard.write(text)
+    return True if false_key and keyboard.is_pressed(false_key) else False
 
 
 def set_hotkeys():
